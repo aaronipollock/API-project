3c92ff1a13b02c5121d9e58b0179d22f95c790f4
Aaron <aaronipollock@gmail.com>
2024-03-27 13:34:40 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
 // backend/routes/index.js
 // ...
 // Add a XSRF-TOKEN cookie
@@ -18,5 +13,12 @@ router.get("/api/csrf/restore", (req, res) => {
     });
   });
   // ...
+
+  // backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
 
 module.exports = router;
