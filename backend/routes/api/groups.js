604a0aab1ab61a8956e9b0e8aff96b8261027225
Aaron <aaronipollock@gmail.com>
2024-05-15 11:23:02 -0700

    comprehensive groups but not sign up

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Op } = require('sequelize');
+const { Op, UUID } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
@@ -7,6 +7,7 @@ const { User, Group, Membership, GroupImage, Venue } = require('../../db/models'
 
 const router = express.Router();
 
+//Get all Groups
 router.get('/', async (req, res, next) => {
     const Groups = await Group.findAll();
 
@@ -46,6 +47,7 @@ router.get('/', async (req, res, next) => {
     });
 });
 
+//Get all Groups joined or organized by the Current User
 router.get('/current', requireAuth, async (req, res, next) => {
     const { user } = req;
 
@@ -104,6 +106,7 @@ router.get('/current', requireAuth, async (req, res, next) => {
     });
 });
 
+//Get details of a Groups from an id
 router.get('/:groupId', async (req, res, next) => {
     const { groupId } = req.params;
 
@@ -111,11 +114,21 @@ router.get('/:groupId', async (req, res, next) => {
         include: [
             { model: GroupImage, attributes: ['id', 'url', 'preview'] },
             { model: User, attributes: ['id', 'firstName', 'lastName'] },
-            { model: Venue, attributes: {
-                exclude: ['createdAt', 'updatedAt']
-            } }
+            {
+                model: Venue, attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                }
+            }
         ]
     });
+
+    if (!group) {
+        return res.status(404).json({
+            errors: {
+                message: "Group couldn't be found"
+            }
+        })
+    }
 
     const memberships = await Membership.findAll({
         attributes: ['groupId', 'status']
@@ -150,6 +163,7 @@ router.get('/:groupId', async (req, res, next) => {
     });
 });
 
+//Create a Group
 router.post('/', requireAuth, async (req, res, next) => {
 
     const { name, about, type, private, city, state } = req.body;
@@ -178,16 +192,39 @@ router.post('/', requireAuth, async (req, res, next) => {
     })
 })
 
+//Add an Image to a Group based on the Group's id
 router.post('/:groupId/images', requireAuth, async (req, res, next) => {
     const { groupId } = req.params;
 
+    const group = await Group.findByPk(groupId);
+
+    // Error if Group does not exist
+    if (!group) {
+        return res.status(404).json({
+            errors: {
+                message: "Group couldn't be found"
+            }
+        })
+    }
+
     const { url, preview } = req.body;
 
-    const newGroupImage = await GroupImage.create({
-        groupId,
-        url,
-        preview,
-    })
+    //Authorization
+    const { user } = req;
+    if (user.id === group.organizerId) {
+        //Add image
+        newGroupImage = await GroupImage.create({
+            groupId,
+            url,
+            preview,
+        })
+    } else {
+        return res.status(401).json({
+            errors: {
+                message: "Only the organizer of the group is authorized to add an image"
+            }
+        })
+    }
 
     return res.json({
         id: newGroupImage.id,
@@ -196,22 +233,39 @@ router.post('/:groupId/images', requireAuth, async (req, res, next) => {
     })
 })
 
+//Edit a Group
 router.put('/:groupId', requireAuth, async (req, res, next) => {
     const { groupId } = req.params;
 
+    // Error if Group does not exist
     const updatedGroup = await Group.findByPk(groupId)
-
+    if (!updatedGroup) {
+        return res.status(404).json({
+            errors: {
+                message: "Group couldn't be found"
+            }
+        })
+    }
 
     const { name, about, type, private, city, state } = req.body;
 
-    await updatedGroup.update({
-        name,
-        about,
-        type,
-        private,
-        city,
-        state,
-    })
+    const { user } = req;
+    if (user.id === updatedGroup.organizerId) {
+        await updatedGroup.update({
+            name,
+            about,
+            type,
+            private,
+            city,
+            state,
+        })
+    } else {
+        return res.status(401).json({
+            errors: {
+                message: "Only the owner of the group is authorized to edit"
+            }
+        })
+    }
 
     return res.json({
         id: updatedGroup.id,
@@ -227,20 +281,40 @@ router.put('/:groupId', requireAuth, async (req, res, next) => {
     })
 })
 
+//Delete a Group
 router.delete('/:groupId', requireAuth, async (req, res, next) => {
     const { groupId } = req.params;
 
-    await Group.destroy(
-        { where:
-            { id: groupId }
-        }
-    )
+    const group = await Group.findByPk(groupId);
+
+    if (!group) {
+        return res.status(404).json({
+            errors: {
+                message: "Group couldn't be found"
+            }
+        })
+    }
+
+    const { user } = req;
+    if (user.id === group.organizerId) {
+        await Group.destroy(
+            {
+                where:
+                    { id: groupId }
+            }
+        )
+    } else {
+        return res.status(401).json({
+            errors: {
+                message: "Only the organizer of the group is authorized to delete"
+            }
+        })
+    }
+
 
     return res.json({
         message: "Successfully deleted"
     })
 });
 
-
-
-    module.exports = router;
+module.exports = router;
