187d294f7d0d5db36713860229a7b1796a666e9a
Aaron <aaronipollock@gmail.com>
2024-05-15 14:25:28 -0700

    basic Edit a Venue by id

--- backend/routes/api/venues.js
+++ backend/routes/api/venues.js
@@ -3,12 +3,66 @@ const { Op, UUID } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Venue } = require('../../db/models');
+const { Venue, Membership, Group, User } = require('../../db/models');
 
 const router = express.Router();
 
-//Get All Venues for a Group soecified by its id
-router.get('')
+// Edit a venue specified by its id
+router.put('/:venueId', requireAuth, async (req, res, next) => {
+    const { venueId } = req.params;
+    const { user } = req;
+
+    const updatedVenue = await Venue.findByPk(venueId);
+
+    if (!updatedVenue) {
+        return res.status(404).json({
+            errors: {
+                message: "Venue couldn't be found"
+            }
+        })
+    };
+
+    const { address, city, state, lat, lng } = req.body;
+
+    const isCohost = await Membership.findAll({
+        where: {
+            groupId: user.id,
+            status: 'co-host'
+        }
+    });
+
+    const isOrganizer = await Group.findAll({
+        where: {
+            organizerId: user.id,
+        }
+    })
+
+    if (isOrganizer.length || isCohost.length) {
+        await updatedVenue.update({
+            address,
+            city,
+            state,
+            lat,
+            lng
+        })
+
+        return res.json({
+            id: updatedVenue.id,
+            groupId: updatedVenue.groupId,
+            address: updatedVenue.address,
+            city: updatedVenue.city,
+            state: updatedVenue.state,
+            lat: updatedVenue.lat,
+            lng: updatedVenue.lng,
+        })
+    } else {
+        return res.status(401).json({
+            errors: {
+                message: "Current User must be the organizer of the group or a member of the group with a status of 'co-host'"
+            }
+        })
+    }
+})
 
 
 
